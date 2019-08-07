ba3adbbdd2587cdfee59787e7eea3a38b069f56e
Andreas Wachter <buddyspencer@protonmail.com>
2019-08-07 11:18:50 +0200

    the hooks stays where it is, changed from bool to "yes"

--- 2019/debian-9/rootfs/run.sh
+++ 2019/debian-9/rootfs/run.sh
@@ -13,7 +13,7 @@ _forwardTerm () {
 
 trap _forwardTerm TERM
 
-if [ "${PHABRICATOR_SSH_PORT_NUMBER}" != "" ] && [ "${PHABRICATOR_SSH_PORT_NUMBER}" != "22" ] ; then
+if [ "${PHABRICATOR_SSH_PORT_NUMBER}" != "" ]; then
     PHABRICATOR_SSH_USER="git"
     if ! id $PHABRICATOR_SSH_USER > /dev/null 2>&1; then
         useradd -m $PHABRICATOR_SSH_USER
@@ -22,14 +22,13 @@ if [ "${PHABRICATOR_SSH_PORT_NUMBER}" != "" ] && [ "${PHABRICATOR_SSH_PORT_NUMBE
 
     echo "$PHABRICATOR_SSH_USER ALL=(daemon) SETENV: NOPASSWD: /opt/bitnami/git/bin/git-upload-pack, /opt/bitnami/git/bin/git-receive-pack" >> /etc/sudoers
 
-    cp /opt/bitnami/phabricator/resources/sshd/phabricator-ssh-hook.sh /usr/share/
-    sed -i s/VCSUSER=.*/VCSUSER=\"$PHABRICATOR_SSH_USER\"/i /usr/share/phabricator-ssh-hook.sh
-    sed -i 's|ROOT=.*|ROOT=\"/opt/bitnami/phabricator\"|i' /usr/share/phabricator-ssh-hook.sh
-    chown root /usr/share/phabricator-ssh-hook.sh
-    chmod 755 /usr/share/phabricator-ssh-hook.sh
+    sed -i s/VCSUSER=.*/VCSUSER=\"$PHABRICATOR_SSH_USER\"/i /opt/bitnami/phabricator/resources/sshd/phabricator-ssh-hook.sh 
+    sed -i 's|ROOT=.*|ROOT=\"/opt/bitnami/phabricator\"|i' /opt/bitnami/phabricator/resources/sshd/phabricator-ssh-hook.sh 
+    chown root /opt/bitnami/phabricator/resources/sshd/phabricator-ssh-hook.sh 
+    chmod 755 /opt/bitnami/phabricator/resources/sshd/phabricator-ssh-hook.sh 
 
     cat /opt/bitnami/phabricator/resources/sshd/sshd_config.phabricator.example > /etc/ssh/sshd_config
-    sed -i 's|AuthorizedKeysCommand .*|AuthorizedKeysCommand /usr/share/phabricator-ssh-hook.sh|i' /etc/ssh/sshd_config
+    sed -i 's|AuthorizedKeysCommand .*|AuthorizedKeysCommand /opt/bitnami/phabricator/resources/sshd/phabricator-ssh-hook.sh|i' /etc/ssh/sshd_config
     sed -i 's/AuthorizedKeysCommandUser .*/AuthorizedKeysCommandUser '$PHABRICATOR_SSH_USER'/i' /etc/ssh/sshd_config
     sed -i 's/AllowUsers.*/AllowUsers '$PHABRICATOR_SSH_USER'/i' /etc/ssh/sshd_config
     sed -i 's/Port.*/Port '$PHABRICATOR_SSH_PORT_NUMBER'/i' /etc/ssh/sshd_config
@@ -38,7 +37,7 @@ if [ "${PHABRICATOR_SSH_PORT_NUMBER}" != "" ] && [ "${PHABRICATOR_SSH_PORT_NUMBE
     service ssh start &
 fi
 
-if [ "${PHABRICATOR_ALLOW_GIT_LFS}" == "true" ] ; then
+if [[ "${PHABRICATOR_ALLOW_GIT_LFS:-no}" == "yes" ]] ; then
     /opt/bitnami/phabricator/bin/config set diffusion.allow-git-lfs true
 fi
 
