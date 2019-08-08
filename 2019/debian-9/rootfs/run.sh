57c63cc94dd3e0bf84f082899a7cb7c70a1c7523
Andreas Wachter <buddyspencer@protonmail.com>
2019-08-08 13:39:32 +0200

    remove pygments, add custom user and group

--- 2019/debian-9/rootfs/run.sh
+++ 2019/debian-9/rootfs/run.sh
@@ -14,13 +14,16 @@ _forwardTerm () {
 trap _forwardTerm TERM
 
 if [ "${PHABRICATOR_SSH_PORT_NUMBER}" != "" ]; then
-    PHABRICATOR_SSH_USER="git"
     if ! id $PHABRICATOR_SSH_USER > /dev/null 2>&1; then
-        useradd -m $PHABRICATOR_SSH_USER
+        useradd $PHABRICATOR_SSH_USER
+        if ! getent group "$PHABRICATOR_SSH_GROUP" > /dev/null 2>&1; then
+            groupadd "$PHABRICATOR_SSH_GROUP" > /dev/null 2>&1
+            usermod -a -G "$PHABRICATOR_SSH_GROUP" "$PHABRICATOR_SSH_USER" > /dev/null 2>&1
+        fi
     fi
-    usermod -p '*' $PHABRICATOR_SSH_USER
+    usermod -p NP $PHABRICATOR_SSH_USER
 
-    echo "$PHABRICATOR_SSH_USER ALL=(daemon) SETENV: NOPASSWD: /opt/bitnami/git/bin/git-upload-pack, /opt/bitnami/git/bin/git-receive-pack" >> /etc/sudoers
+    echo "$PHABRICATOR_SSH_USER ALL=(phabricator) SETENV: NOPASSWD:/opt/bitnami/git/bin/git-upload-pack,/opt/bitnami/git/bin/git-receive-pack" >> /etc/sudoers
 
     sed -i s/VCSUSER=.*/VCSUSER=\"$PHABRICATOR_SSH_USER\"/i /opt/bitnami/phabricator/resources/sshd/phabricator-ssh-hook.sh 
     sed -i 's|ROOT=.*|ROOT=\"/opt/bitnami/phabricator\"|i' /opt/bitnami/phabricator/resources/sshd/phabricator-ssh-hook.sh 
@@ -32,13 +35,18 @@ if [ "${PHABRICATOR_SSH_PORT_NUMBER}" != "" ]; then
     sed -i 's/AuthorizedKeysCommandUser .*/AuthorizedKeysCommandUser '$PHABRICATOR_SSH_USER'/i' /etc/ssh/sshd_config
     sed -i 's/AllowUsers.*/AllowUsers '$PHABRICATOR_SSH_USER'/i' /etc/ssh/sshd_config
     sed -i 's/Port.*/Port '$PHABRICATOR_SSH_PORT_NUMBER'/i' /etc/ssh/sshd_config
-    sed -i 's;^\(git:.*\):/home/phabricator:;\1:/opt/bitnami/phabricator:;' /etc/passwd
-    
+    if [ "${PHABRICATOR_SSH_USER}" == "git" ]; then
+        sed -i 's;^\('$PHABRICATOR_SSH_USER''':.*\):/home/phabricator:;\1:/opt/bitnami/phabricator:;' /etc/passwd
+    else
+        sed -i 's;^\('$PHABRICATOR_SSH_USER''':.*\):/home/'$PHABRICATOR_SSH_USER':;\1:/opt/bitnami/phabricator:;' /etc/passwd
+    fi
+
     if [[ ! -h "/usr/bin/php"  ]]; then
         ln -s /opt/bitnami/php/bin/php /usr/bin/php
     fi
 
     /opt/bitnami/phabricator/bin/config set diffusion.ssh-port $PHABRICATOR_SSH_PORT_NUMBER
+    /opt/bitnami/phabricator/bin/config set diffusion.ssh-user $PHABRICATOR_SSH_USER
     service ssh start &
 fi
 
