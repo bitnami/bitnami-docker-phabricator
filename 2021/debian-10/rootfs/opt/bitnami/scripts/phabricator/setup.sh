43a5d65f095c2a463b712589f6f5e220b4b0e1fc
juan131 <juanariza@vmware.com>
2021-02-23 12:54:59 +0100

    Fix 'command not found' errors under different circustamces

--- 2021/debian-10/rootfs/opt/bitnami/scripts/phabricator/setup.sh
+++ 2021/debian-10/rootfs/opt/bitnami/scripts/phabricator/setup.sh
@@ -43,10 +43,15 @@ if am_i_root; then
     debug_execute usermod -p NP "$PHABRICATOR_SSH_VCS_USER"
     # Web Server & VCS users need to be able to sudo as the PH daemon user so they can interact with repositories
     cat > /etc/sudoers.d/phabricator << EOF
-"${WEB_SERVER_DAEMON_USER}" ALL=("${PHABRICATOR_DAEMON_USER}") SETENV: NOPASSWD: $(command -v git), $(command -v git-http-backend), $(command -v ssh)
-"${PHABRICATOR_SSH_VCS_USER}" ALL=("${PHABRICATOR_DAEMON_USER}") SETENV: NOPASSWD: $(command -v git), $(command -v git-upload-pack), $(command -v git-receive-pack), $(command -v ssh)
+${WEB_SERVER_DAEMON_USER} ALL=(${PHABRICATOR_DAEMON_USER}) SETENV: NOPASSWD: $(command -v git), $(command -v git-http-backend), $(command -v ssh)
+${PHABRICATOR_SSH_VCS_USER} ALL=(${PHABRICATOR_DAEMON_USER}) SETENV: NOPASSWD: $(command -v git), $(command -v git-upload-pack), $(command -v git-receive-pack), $(command -v ssh)
 EOF
     chmod 440 /etc/sudoers.d/phabricator
+    # SSH + sudo combination resets the environment and we're finding a lot of issues related to command unable to find binaries
+    # TODO: find a better way to define the envionment for daemon + vcs users combined with sudo actions.
+    for binary in "php" "git" "git-http-backend" "git-upload-pack" "git-receive-pack"; do
+        ln -s "$(command -v "$binary")" "/usr/bin/${binary}"
+    done
 
     # Ensure required directories exists and have proper permissions
     info "Configuring file permissions for Phabricator"
